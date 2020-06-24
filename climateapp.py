9c6cc0a5f2374d5d46bc6e4fb1bf3746186ec1e1
KurtReinke <63032355+KurtReinke@users.noreply.github.com>
2020-06-23 18:50:04 -0700

    Update climateapp.py

--- climateapp.py
+++ climateapp.py
@@ -1,27 +1,25 @@
 #import flask and others
 import numpy as np
-
 import sqlalchemy
 from sqlalchemy.ext.automap import automap_base
 from sqlalchemy.orm import Session
 from sqlalchemy import create_engine, func
-
 from flask import Flask, jsonify
 
 #Engine
 engine = create_engine("sqlite:///hawaii.sqlite")
 Base = automap_base()
 Base.prepare(engine, reflect=True)
+#assign Vars
 me = Base.classes.measurements
 st = Base.classes.station
-
 #create the app
 app1 = Flask(_name_)
 
 
 #Flask Routes
 @app.route("/")
-def welcome():
+def Intro():
     return (
         f"Available API Routes:<br/>"
         f"<br/>"
@@ -31,47 +29,46 @@ def welcome():
         f"<br/>"
         f"/api/v1.0/tobs<br/>"
         f"<br/>"
+        f"/api/v1.0/<start>"
+        f"<br/>"
         f"/api/v1.0/<start>/<end><br/>"   
         f"<br/>")  
-
 
 @app.route("/api/v1.0/precipitation")
 def precipitation():
     date=session.query(me.date).order_by(me.date.desc()).first()
-    yearly=dt.date(2017, 8, 23) - dt.timedelta(days=364)
-    prec2=session.query(me.date).filter(me.date >= yearly).order_by(me.date.desc).all()
-
-    prec_df=list(prec2)
+    yeartodate=dt.date(2017, 8, 23) - dt.timedelta(days=364)
+    prec2=session.query(me.prcp).filter(me.date >= yeartodate).order_by(me.date.desc()).all()
+    prec_df=dict(prec2)
     return jsonify(prec_df)
-
 
 @app.route("/api/v1.0/stations")
 def stations():
-    st_df= session.query(st.name, st.station)
+    st_df= session.query(st.date, st.station)
     st_df= pd.read_sql(st_df.statement)
-
     return jsonify(st_df)
 
 @app.route("/api/v1.0/tobs")
 def tobs():
     max_date = session.query(me.date).order_by(me.date.desc()).first()
-    yeartodate=dt.datetime(2017, 8, 23) - dt.timedelta(days=365)
-    tobs_df = session.query(me.date, me.tobs).filter(me.date >= yeartodate).all()
+    yeartodate2=dt.datetime(2017, 8, 23) - dt.timedelta(days=365)
+    tobs_df = session.query(me.date, me.tobs).filter(me.date >= yeartodate).order_by(me.date.desc()).all()
     tobs_df = list(tobs_df)
-
     return jsonify(tobs_list)
+
 
 @app.route("/api/v1.0/<start>")
 def start():
-    dates2 = session.query(me.date, func.min(me.tobs), func.max(me.tobs), func.avg(me.tobs)).filter(me.date >= start).group_by(me.date).all()
+    yeartodate3=dt.datetime(2017, 8, 23) - dt.timedelta(days=365)
+    dates2 = session.query(me.date, func.min(me.tobs), func.max(me.tobs), func.avg(me.tobs)).filter(me.date >= yeartodate).group_by(me.date).all()
     dates2_df=list(dates2)
     return jsonify(dates2_df)
 
 @app.route("/api/v1.0/<start>/<end>")
 def start_and_end(start=None, end=None):
-    dates3 = session.query(me.date, func.min(me.tobs), func.max(me.tobs), func.avg(me.tobs)).filter(me.date >= start).filter(me.date <= end).group_by(me.date).all()
-    dates3_df=list(dates3)
-    return jsonify(dates3_df)
+    dates4 = session.query(me.date, func.min(me.tobs), func.max(me.tobs), func.avg(me.tobs)).filter(me.date >= start).filter(me.date <= end).group_by(me.date).all()
+    dates4_df=list(dates4)
+    return jsonify(dates4_df)
 
 if __name__ == '__main__':
     app.run(debug=True)
